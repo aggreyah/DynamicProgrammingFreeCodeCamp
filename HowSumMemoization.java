aad7d1c10408c264ccebbdc0681db76faeb967cc
aggreyah <aggreyah@gmail.com>
2022-04-03 07:35:13 +0300

    added the memoization & recursive implementations of the howSum problem.

---
+++ HowSumMemoization.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HowSumMemoization {
+    public static void main(String[] args) {
+        System.out.println(howSum(7, new int[]{2, 3}, new HashMap<>()));
+        System.out.println(howSum(7, new int[]{5, 3, 4, 7}, new HashMap<>()));
+        System.out.println(howSum(7, new int[]{2, 4}, new HashMap<>()));
+        System.out.println(howSum(8, new int[]{2, 3, 5}, new HashMap<>()));
+        System.out.println(howSum(5321, new int[]{7, 14}, new HashMap<>()));
+    }
+
+    private static List<Integer> howSum(int targetSum, int[] ints, Map<Integer, List<Integer>> map) {
+        if (map.containsKey(targetSum)) return map.get(targetSum);
+        if (targetSum == 0) return new ArrayList<>();
+        if (targetSum < 0) return null;
+
+        for (int item : ints){
+            List<Integer> remainderResult = howSum(targetSum - item, ints, map);
+            if (remainderResult != null){
+                remainderResult.add(item);
+                map.put(targetSum, remainderResult);
+                return map.get(targetSum);
+            }
+        }
+        map.put(targetSum, null);
+        return null;
+    }
+}
---
+++ HowSumRecursion.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class HowSumRecursion {
+    public static void main(String[] args) {
+        System.out.println(howSum(7, new int[]{2, 3}));
+        System.out.println(howSum(7, new int[]{5, 3, 4, 7}));
+        System.out.println(howSum(7, new int[]{2, 4}));
+        System.out.println(howSum(8, new int[]{2, 3, 5}));
+        System.out.println(howSum(300, new int[]{7, 14}));
+    }
+
+    private static List<Integer> howSum(int targetSum, int[] ints) {
+        if (targetSum == 0) return new ArrayList<>();
+        if (targetSum < 0) return null;
+
+        for (int item : ints){
+            List<Integer> remainderResult = howSum(targetSum - item, ints);
+            if (remainderResult != null){
+                remainderResult.add(item);
+                return remainderResult;
+            }
+        }
+        return null;
+    }
+}
